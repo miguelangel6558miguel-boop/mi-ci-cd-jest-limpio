fe9d67a4328bd373f670e286a307472eb89f7d3e
miguelangel6558miguel-boop <miguelangel6558miguel@gmail.com>
2025-09-25 04:13:18 -0500

    Create hola-cyber.js
    Agrego código principal con export

---
+++ hola-cyber.js
@@ -0,0 +1,9 @@
+console.log("¡Hola! Este es mi primer tool de ciberseguridad. Scan activado sin errores.");
+console.log("¡Scan cyber exitoso!");
+
+// Función simple para unit test (simula un "scan" cyber)
+function scanSeguro(texto) {
+  return texto + " es seguro, sin virus.";
+}
+
+module.exports = { scanSeguro };
